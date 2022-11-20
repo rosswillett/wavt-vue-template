b1896848d1db1ab67b7a296199f40f4c62087595
Ross Willett <ross@willettavt.com>
2022-11-19 17:11:08 -0800

    Modernize package versions, configure package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,35 @@
 module.exports = {
-  env: {
-    node: true,
-    es6: true,
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-  ],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    parser: "@typescript-eslint/parser",
-    sourceType: "module",
-  },
-  rules: {
-    "vue/multi-word-component-names": "off",
-    "no-unused-vars": "warn",
-    "no-debugger": "warn",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "vue/html-closing-bracket-newline": ["error", {
-      "singleline": "never",
-      "multiline": "never"
-    }],
-  },
+    env: {
+        node: true,
+        es6: true,
+    },
+    extends: [
+        "plugin:vue/vue3-essential",
+        "plugin:vue/vue3-strongly-recommended",
+        "eslint:recommended",
+        "@vue/typescript/recommended",
+        "@vue/prettier",
+    ],
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        parser: "@typescript-eslint/parser",
+        sourceType: "module",
+    },
+    rules: {
+        "vue/multi-word-component-names": "off",
+        'vue/no-unused-vars': 'warn',
+        "no-debugger": "warn",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/ban-ts-comment": "off",
+        "vue/html-closing-bracket-newline": ["error", {
+            "singleline": "never",
+            "multiline": "never"
+        }],
+        /* eslint-disable @typescript-eslint/no-unused-vars */
+    },
 };
